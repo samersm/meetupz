b5a083a3958bc13c74f4c641656d6495f6d37874
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-01-10 23:32:20 +0300

    Added single view item the new syntax

--- client-ng-src/src/app/components/meetup-details/meetup-details.component.ts
+++ client-ng-src/src/app/components/meetup-details/meetup-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Meetup } from '../../meetup';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-meetup-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meetup-details.component.css']
 })
 export class MeetupDetailsComponent implements OnInit {
+  meetups: Meetup;
+  sub:any;
 
-  constructor() { }
+  constructor(    public dataService:DataService,
+                  public router:Router,
+                  public route:ActivatedRoute
+                ) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = params['id'];
+      console.log('getting meetup with id: ', id);
+      this.dataService
+        .getMeetup(id)
+        .subscribe(meetups => {this.meetups = meetups});
+    });
   }
 
 }
--- client-ng-src/src/app/services/data.service.ts
+++ client-ng-src/src/app/services/data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Meetup } from '../meetup';
 
 @Injectable()
 export class DataService {
@@ -8,10 +11,22 @@ export class DataService {
   constructor(public http: Http) {
     console.log('Data service connected...');
    }
-
+   public getHeaders(){
+     // I included these headers because otherwise FireFox
+     // will request text/html
+     let headers = new Headers();
+     headers.append('Accept', 'application/json');
+     return headers;
+   }
    getMeetups() {
     return this.http.get('http://localhost:3000/api/meetups')
       .map((res:Response) => res.json());
+    }
+   getMeetup(id: string): Observable<Meetup> {
+     let meetup$ = this.http
+      .get(`http://localhost:3000/api/meetups/${id}`, {headers: this.getHeaders()})
+      .map((res:Response) => res.json());
+      return meetup$;
     }
 
 }
